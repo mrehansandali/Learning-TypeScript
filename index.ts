c63c6d59eee166d9f4b3518cfeeb2b739563d330
mrehansandali <mrehansandali@gmail.com>
2023-06-24 01:06:49 +0500

    Start Learning TypeScript

---
+++ index.ts
@@ -0,0 +1,21 @@
+// const button = document.querySelector("button")!;
+// const inp1 = document.getElementById("num1")! as HTMLInputElement;
+// const inp2 = document.getElementById("num2")! as HTMLInputElement;
+
+// function addition (num1:number, num2:number) {
+//     return num1 + num2
+// }
+
+
+// button.addEventListener("click", function(){
+//     console.log(addition (+inp1.value, +inp2.value))
+// })
+
+
+
+function add(n1:number, n2:number){
+    return n1 + n2
+}
+const num1 = 5;
+const num2 = 4;
+console.log(add(num1,num2))
